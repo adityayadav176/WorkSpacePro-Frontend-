d0df93e133372363338b21c45cdb4d1ccdaef70d
adityayadav176 <ay6598aditya@gmail.com>
2026-02-25 14:16:17 +0530

    In Dashboard component fetch all task and notes length and completed task

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, { useContext, useEffect } from 'react'
 import NavBar from '../components/NavBar';
 import Footer from './footer';
 import "./Css/dashboard.css";
@@ -6,67 +6,71 @@ import noteContext from '../context/notes/noteContext';
 import taskContext from '../context/tasks/taskContext';
 
 function Home() {
-   const NotesContext = useContext(noteContext);
-    const {notes} = NotesContext;
-   const TaskContext = useContext(taskContext);
-    const {task} = TaskContext;
-     
+  const NotesContext = useContext(noteContext);
+  const { notes, getNotes } = NotesContext;
+  const TaskContext = useContext(taskContext);
+  const { task, getTask } = TaskContext;
+  useEffect(() => {
+   getNotes()
+   getTask()
+  }, [])
+
   return (
     <>
-    <NavBar />
-     <div className="tasks-title-container">
-      <div className="tasks-nav">
-        <h2>Dashboard Overview</h2>
-        <p>Monitor your productivity and activity</p>
-      </div>
+      <NavBar />
+      <div className="tasks-title-container">
+        <div className="tasks-nav">
+          <h2>Dashboard Overview</h2>
+          <p>Monitor your productivity and activity</p>
+        </div>
       </div>
       <div className="dashboard-cards">
-      
-      <div className="card">
-        <div className="card-text">
-          <p>Total Tasks</p>
-          <h2>{task.length}</h2>
-        </div>
-        <div className="icon blue">
-          <i className=
-"fa-regular fa-circle-check"></i>
-        </div>
-      </div>
 
-      <div className="card">
-        <div className="card-text">
-          <p>Notes Created</p>
-          <h2>{notes.length}</h2>
+        <div className="card">
+          <div className="card-text">
+            <p>Total Tasks</p>
+            <h2>{task?.length || 0}</h2>
+          </div>
+          <div className="icon blue">
+            <i className=
+              "fa-regular fa-circle-check"></i>
+          </div>
         </div>
-        <div className="icon purple">
-          <i className="fa-regular fa-file-lines"></i>
-        </div>
-      </div>
 
-      <div className="card">
-        <div className="card-text">
-          <p>Completed</p>
-          <h2>1</h2>
+        <div className="card">
+          <div className="card-text">
+            <p>Notes Created</p>
+            <h2>{notes?.length}</h2>
+          </div>
+          <div className="icon purple">
+            <i className="fa-regular fa-file-lines"></i>
+          </div>
         </div>
-        <div className="icon green">
-         <i className=
-"fa-solid fa-arrow-trend-up"></i>
-        </div>
-      </div>
 
-      <div className="card">
-        <div className="card-text">
-          <p>Productivity</p>
-          <h2>33%</h2>
+        <div className="card">
+          <div className="card-text">
+            <p>Completed</p>
+            <h2>{task.filter(task => task.status === "Complete").length}</h2>
+          </div>
+          <div className="icon green">
+            <i className=
+              "fa-solid fa-arrow-trend-up"></i>
+          </div>
         </div>
-        <div className="icon orange">
-          <i className=
-"fa-solid fa-chart-column"></i>
-        </div>
-      </div>
 
-    </div>
-    <Footer/>
+        <div className="card">
+          <div className="card-text">
+            <p>Productivity</p>
+            <h2>33%</h2>
+          </div>
+          <div className="icon orange">
+            <i className=
+              "fa-solid fa-chart-column"></i>
+          </div>
+        </div>
+
+      </div>
+      <Footer />
     </>
   )
 }
